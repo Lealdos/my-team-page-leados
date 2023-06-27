494670af2b87892a5819664ff22639b7f8295593
Jose Delgado <j3leonardodelgado@gmail.com>
2023-06-27 13:18:21 -0400

    fixed (index.html path )

--- src/js/mapImag.js
+++ src/js/mapImag.js
@@ -1,10 +1,10 @@
 const imgs = [
-  { src: "./src/img/photo1.png", tittle: "Product owner", name: "Bill Mahoney" },
-  { src: "./src/img/developer.jpg", tittle: "Developer", name: "Jose Delgado" },
-  { src: "./src/img/photo2.png", tittle: "Art director ", name: "Saba Cabrera" },
-  { src: "./src/img/photo4.png", tittle: "Skylah Lu", name: "Skylah Lu" },
-  { src: "./src/img/photo3.png", tittle: "Tech Lead", name: "Skylah Lu" },
-  { src: "./src/img/photo5.png", tittle: "Developer", name: "Griff Richards" },
+  { src: "/src/img/photo1.png", tittle: "Product owner", name: "Bill Mahoney" },
+  { src: "/src/img/developer.jpg", tittle: "Developer", name: "Jose Delgado" },
+  { src: "/src/img/photo2.png", tittle: "Art director ", name: "Saba Cabrera" },
+  { src: "/src/img/photo4.png", tittle: "Skylah Lu", name: "Skylah Lu" },
+  { src: "/src/img/photo3.png", tittle: "Tech Lead", name: "Skylah Lu" },
+  { src: "/src/img/photo5.png", tittle: "Developer", name: "Griff Richards" },
 ];
 
 const galeryID = document.getElementById("galery");
